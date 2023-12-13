138bd78671458bdae6841a78058fa4979b729733
Hawii02 <leon.onditi@student.moringaschool.com>
2023-12-13 12:05:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2; 
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() + 1);
+ let num3 = 7;
+ let num4 = 25;
+let mod = num4 % num3;
+
+let max = Math.max(15,16,17,20);
+
